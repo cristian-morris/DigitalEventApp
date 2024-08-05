35c82c8849eada1ef9f32874efe64b471126ab47
D3nn1S-Mugart3 <dennis.mugarte05@gmail.com>
2024-08-04 19:09:09 -0600

    Cambios en la ventana de detalles de eventos

--- lib/pages/event_detail_page.dart
+++ lib/pages/event_detail_page.dart
@@ -1,4 +1,3 @@
-// event_detail_page.dart
 import 'dart:convert';
 import 'package:digitalevent/models/evento.dart';
 import 'package:flutter/foundation.dart';
@@ -8,15 +7,14 @@ import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 
 class EventDetailPage extends StatelessWidget {
-
-   Future<String> createPaymentIntent(int amount, String currency) async {
+  Future<String> createPaymentIntent(int amount, String currency) async {
     try {
       final body = jsonEncode({
         'amount': amount,
         'currency': currency,
       });
       final response = await http.post(
-        Uri.https('api-digitalevent.onrender.com', '/api/pagos/pago'), // Asegúrate de usar tu URL correcta
+        Uri.https('api-digitalevent.onrender.com', '/api/pagos/pago'),
         body: body,
         headers: {'Content-Type': 'application/json'},
       );
@@ -24,7 +22,7 @@ class EventDetailPage extends StatelessWidget {
       if (response.statusCode == 200) {
         final paymentIntentData = jsonDecode(response.body);
         final clientSecret = paymentIntentData['client_secret'] as String;
-        print(clientSecret); // Asegúrate de que este print sea solo para depuración
+        print(clientSecret);
         return clientSecret;
       } else {
         throw Exception('Failed to create payment intent');
@@ -43,10 +41,9 @@ class EventDetailPage extends StatelessWidget {
 
       await Stripe.instance.initPaymentSheet(
         paymentSheetParameters: SetupPaymentSheetParameters(
-          paymentIntentClientSecret: paymentIntentClientSecret,
-          style: ThemeMode.light,
-          merchantDisplayName: 'ejemplo'
-        ),
+            paymentIntentClientSecret: paymentIntentClientSecret,
+            style: ThemeMode.light,
+            merchantDisplayName: 'ejemplo'),
       );
 
       await displayPaymentSheet(context);
@@ -80,42 +77,145 @@ class EventDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(evento.nombreEvento),
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(evento.imagenUrl,
-                height: 200, width: double.infinity, fit: BoxFit.cover),
-            SizedBox(height: 8),
-            Text(evento.nombreEvento,
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            SizedBox(height: 8),
-            Text(DateFormat('yyyy-MM-dd – kk:mm').format(evento.fechaInicio),
-                style: TextStyle(color: Colors.grey)),
-            SizedBox(height: 8),
-            Text(evento.ubicacion, style: TextStyle(color: Colors.grey)),
-            SizedBox(height: 8),
-            Text('Detalles del Evento',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            SizedBox(height: 8),
-            Text('Tipo de Evento: ${evento.tipoEvento}'),
-            Text('Organizado por: ${evento.organizadorNombre}'),
-            Text('Categoría: ${evento.categoriaNombre}'),
-            Text('Estado: ${evento.estado}'),
-            Text(
-                'Fecha de Autorización: ${DateFormat('yyyy-MM-dd').format(evento.fechaAutorizacion)}'),
-            ElevatedButton(
-              onPressed: () => makePayment(context),
-              child: Text("Comprar Boleto"),
-            )
-          ],
+        title: Text(
+          evento.nombreEvento,
+          style:
+              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-          )
-        ],
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.purple, Colors.purpleAccent],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image.network(evento.imagenUrl,
+                    height: 200, width: double.infinity, fit: BoxFit.cover),
+              ),
+              const SizedBox(height: 16),
+              Text(evento.nombreEvento,
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Icon(Icons.calendar_today, color: Colors.grey),
+                  const SizedBox(width: 4),
+                  Text(
+                    DateFormat('yyyy-MM-dd – kk:mm').format(evento.fechaInicio),
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Icon(Icons.location_on, color: Colors.grey),
+                  const SizedBox(width: 4),
+                  Text(evento.ubicacion,
+                      style: const TextStyle(color: Colors.grey)),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Divider(color: Colors.grey),
+              const SizedBox(height: 16),
+              const Text('Detalles del Evento',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Icon(Icons.category, color: Colors.grey),
+                  const SizedBox(width: 4),
+                  Text('Categoría: ${evento.categoriaNombre}',
+                      style: TextStyle(color: Colors.grey[700])),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Icon(Icons.event, color: Colors.grey),
+                  const SizedBox(width: 4),
+                  Text('Tipo de Evento: ${evento.tipoEvento}',
+                      style: TextStyle(color: Colors.grey[700])),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Icon(Icons.person, color: Colors.grey),
+                  const SizedBox(width: 4),
+                  Text('Organizado por: ${evento.organizadorNombre}',
+                      style: TextStyle(color: Colors.grey[700])),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Icon(Icons.info, color: Colors.grey),
+                  const SizedBox(width: 4),
+                  Text('Estado: ${evento.estado}',
+                      style: TextStyle(color: Colors.grey[700])),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  const Icon(Icons.check_circle, color: Colors.grey),
+                  const SizedBox(width: 4),
+                  Text(
+                      'Autorizado el: ${DateFormat('yyyy-MM-dd').format(evento.fechaAutorizacion)}',
+                      style: TextStyle(color: Colors.grey[700])),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [
+                        Colors.purple,
+                        Colors.deepPurple,
+                        Colors.purpleAccent
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: ElevatedButton.icon(
+                    onPressed: () => makePayment(context),
+                    icon: const Icon(Icons.payment),
+                    label: const Text("Comprar Boleto"),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.transparent,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      textStyle: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
