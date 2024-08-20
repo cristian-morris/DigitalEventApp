ffa7a8a4115a5ab053978c6736e6ce0f45ac3c98
antoniochi90 <160528309+antoniochi90@users.noreply.github.com>
2024-08-20 13:34:24 -0600

    actualizacion del codigo historial_pago.dart

--- lib/pages/historial_pago.dart
+++ lib/pages/historial_pago.dart
@@ -4,6 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:digitalevent/auth_provider.dart';
 
 class PaymentHistory {
   final int pagoId;
@@ -51,22 +53,35 @@ class PaymentHistory {
   }
 }
 
-Future<List<PaymentHistory>> fetchPaymentHistory() async {
+Map<int, String> tipoPagoMap = {
+  1: 'Tarjeta de Crédito',
+  2: 'Tarjeta de Débito',
+  3: 'Transferencia Bancaria',
+  // Agrega más tipos de pago según sea necesario
+};
+
+Future<List<PaymentHistory>> fetchPaymentHistory(
+    int usuarioId, String token) async {
   try {
     final response = await http.get(
       Uri.parse(
-          'https://api-digitalevent.onrender.com/api/pagos/historialpagos'),
+          'https://api-digital.onrender.com/api/pagos/historial/$usuarioId'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
     );
 
+    print("API Response: ${response.body}"); // Muestra la respuesta de la API
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => PaymentHistory.fromJson(json)).toList();
     } else {
-      throw Exception('Failed to load payment history');
+      throw Exception('Fallo al obtener sus historial de pagos');
     }
   } catch (e) {
     print('Exception caught: $e');
-    throw Exception('Failed to load payment history');
+    throw Exception('Fallo al obtener su historial de pagos');
   }
 }
 
@@ -83,7 +98,29 @@ class _historialPagoState extends State<historialPago> {
   @override
   void initState() {
     super.initState();
-    futurePaymentHistory = fetchPaymentHistory();
+    _initializePaymentHistory();
+  }
+
+  Future<void> _initializePaymentHistory() async {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    final usuarioId = authProvider.user?['usuario_id'];
+    final token = authProvider.token;
+
+    // Verifica el ID del usuario y el token
+    print("Usuario ID: $usuarioId");
+    print("Token: $token");
+
+    if (usuarioId != null && token != null) {
+      setState(() {
+        futurePaymentHistory = fetchPaymentHistory(usuarioId, token);
+      });
+    } else {
+      // Muestra un mensaje de error si no se puede obtener el ID del usuario o el token
+      setState(() {
+        futurePaymentHistory =
+            Future.error('No se pudo obtener el ID del usuario o el token.');
+      });
+    }
   }
 
   String formatDate(DateTime date) {
@@ -94,6 +131,14 @@ class _historialPagoState extends State<historialPago> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.purple, Colors.purpleAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+        ),
         title: const Center(
           child: Text(
             'Historial de Pagos',
@@ -109,179 +154,155 @@ class _historialPagoState extends State<historialPago> {
             Navigator.of(context).pop();
           },
         ),
-        backgroundColor: Colors.deepPurple,
       ),
       body: FutureBuilder<List<PaymentHistory>>(
         future: futurePaymentHistory,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No hay datos disponibles'));
+            return const Center(child: Text('No hay datos disponibles'));
           } else {
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 var payment = snapshot.data![index];
                 return Card(
-                  color: Colors.deepPurple[400],
+                  color: const Color.fromARGB(255, 255, 255, 255),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(15.0),
                   ),
-                  margin: EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.all(8.0),
                   elevation: 8,
                   child: Padding(
-                    padding: const EdgeInsets.all(15.0),
+                    padding: const EdgeInsets.all(13.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Icon(
-                              Icons.attach_money,
-                              color: Color(0xFF81e665),
-                            ),
-                            SizedBox(width: 8.0),
-                            Text(
-                              'Monto: ${payment.monto} MXN',
-                              style: GoogleFonts.openSans(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
+                            Padding(
+                                padding: const EdgeInsets.only(top: 1.0),
+                                child: Image.asset(
+                                  'assets/card-bank.png',
+                                  height: 185.0,
+                                  fit: BoxFit.cover,
+                                ))
                           ],
                         ),
-                        SizedBox(height: 10.0),
+                        const Divider(
+                          color: Colors.grey,
+                        ),
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Icon(
-                              Icons.event,
-                              color: Colors.white,
+                            const Icon(
+                              Icons.calendar_month_sharp,
+                              size: 22,
+                              color: Colors.black,
                             ),
-                            SizedBox(width: 8.0),
                             Text(
                               payment.fecha != null
                                   ? 'Fecha: ${formatDate(payment.fecha!)}'
                                   : "sin datos",
                               style: GoogleFonts.openSans(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 10.0),
-                        Row(
-                          children: [
-                            FaIcon(
-                              FontAwesomeIcons.solidCreditCard,
-                              color: Colors.purpleAccent,
-                            ),
-                            SizedBox(width: 8.0),
-                            Text(
-                              payment.tipoPagoId != null
-                                  ? 'Tipo de Pago: ${payment.tipoPagoId}'
-                                  : "sin datos",
-                              style: GoogleFonts.openSans(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 10.0),
-                        Row(
-                          children: [
-                            FaIcon(
-                              FontAwesomeIcons.solidUser,
-                              color: Colors.purpleAccent,
-                            ),
-                            SizedBox(width: 8.0),
-                            Text(
-                              payment.usuarioId != null
-                                  ? 'Usuario: ${payment.usuarioId}'
-                                  : "sin datos",
-                              style: GoogleFonts.openSans(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        if (payment.fInicioEp != null) SizedBox(height: 10.0),
-                        if (payment.fInicioEp != null)
-                          Row(
-                            children: [
-                              FaIcon(FontAwesomeIcons.calendarDay),
-                              SizedBox(width: 8.0),
-                              Text(
-                                payment.fInicioEp != null
-                                    ? 'Fecha Inicio EP: ${formatDate(payment.fInicioEp!)}'
-                                    : "sin datos",
-                                style: GoogleFonts.openSans(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold),
+                                color: Colors.black,
+                                fontSize: 16,
                               ),
-                            ],
-                          ),
-                        if (payment.fFinEp != null) SizedBox(height: 10.0),
-                        if (payment.fFinEp != null)
-                          Row(
-                            children: [
-                              FaIcon(FontAwesomeIcons.calendarDay),
-                              SizedBox(width: 8.0),
-                              Text(
-                                payment.fFinEp != null
-                                    ? 'Fecha Fin EP: ${formatDate(payment.fFinEp!)}'
-                                    : "sin datos",
-                                style: GoogleFonts.openSans(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        SizedBox(height: 10.0),
+                            ),
+                            const Spacer(),
+                            Text(
+                              '\$${payment.monto} MXN',
+                              style: GoogleFonts.openSans(
+                                  color: Colors.green,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10.0),
                         Row(
                           children: [
-                            FaIcon(
-                              FontAwesomeIcons.solidCreditCard,
-                              color: Colors.yellowAccent,
+                            Image.asset(
+                              'assets/metodoPagos.png',
+                              width: 30,
+                              height: 30,
                             ),
-                            SizedBox(width: 8.0),
+                            const SizedBox(width: 8.0),
+                            Text(
+                              'Pago por: ${tipoPagoMap[payment.tipoPagoId] ?? 'Sin datos'}', // Concatenar el texto fijo con el valor descriptivo
+                              style: GoogleFonts.openSans(
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 10.0),
+                        Row(
+                          children: [
+                            const FaIcon(
+                              FontAwesomeIcons.creditCard,
+                              color: Colors.black,
+                            ),
+                            const SizedBox(width: 8.0),
                             Text(
                               payment.numeroTarjeta != null
                                   ? 'Número de Tarjeta: ${payment.numeroTarjeta}'
                                   : "sin datos",
                               style: GoogleFonts.openSans(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
                             ),
                           ],
                         ),
-                        SizedBox(height: 10.0),
+                        const SizedBox(height: 10.0),
                         Row(
                           children: [
-                            FaIcon(
-                              FontAwesomeIcons.calendarDay,
-                              color: Colors.yellowAccent,
+                            const FaIcon(
+                              FontAwesomeIcons.solidUser,
+                              color: Colors.black,
+                              size: 22,
                             ),
-                            SizedBox(width: 8.0),
+                            const SizedBox(width: 8.0),
                             Text(
-                              payment.fechaExpiracion != null
-                                  ? 'Fecha Expiración: ${formatDate(payment.fechaExpiracion!)}'
+                              payment.usuarioId != null
+                                  ? 'Usuario: ${payment.usuarioId}'
                                   : "sin datos",
                               style: GoogleFonts.openSans(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
                             ),
                           ],
                         ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.circleCheck,
+                              color: Colors.green,
+                              size: 45,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Pago Completado',
+                              style: TextStyle(color: Colors.green),
+                            )
+                          ],
+                        )
                       ],
                     ),
                   ),
